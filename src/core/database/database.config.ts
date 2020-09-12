1f628b541f92eaa64a0647e0a5f874f98eb600c1
nokazn <nokazn@gmail.com>
2020-09-12 11:59:24 +0900

    modify database config

--- src/core/database/database.config.ts
+++ src/core/database/database.config.ts
@@ -3,29 +3,53 @@ import { IDatabaseConfig } from './interfaces/dbConfig.interface';
 
 dotenv.config();
 
+const {
+  DB_USER_NAME,
+  DB_PASSWORD,
+  DB_NAME_DEVELOPMENT,
+  DB_NAME_PRODUCTION,
+  DB_NAME_TEST,
+  DB_HOST,
+  DB_PORT,
+} = process.env;
+
+if (
+  DB_USER_NAME == null ||
+  DB_PASSWORD == null ||
+  DB_NAME_DEVELOPMENT == null ||
+  DB_NAME_PRODUCTION == null ||
+  DB_NAME_TEST == null ||
+  DB_HOST == null ||
+  DB_PORT == null
+) {
+  const message = '環境変数が設定されていません。';
+  console.error(message, process.env);
+  throw new Error(message);
+}
+
 export const databaseConfig: IDatabaseConfig = {
   development: {
-    username: process.env.DB_USER_NAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME_DEVELOPMENT,
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT, 10),
+    username: DB_USER_NAME,
+    password: DB_PASSWORD,
+    database: DB_NAME_DEVELOPMENT,
+    host: DB_HOST,
+    port: parseInt(DB_PORT, 10),
     dialect: 'postgres',
   },
   test: {
-    username: process.env.DB_USER_NAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME_TEST,
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT, 10),
+    username: DB_USER_NAME,
+    password: DB_PASSWORD,
+    database: DB_NAME_TEST,
+    host: DB_HOST,
+    port: parseInt(DB_PORT, 10),
     dialect: 'postgres',
   },
   production: {
-    username: process.env.DB_USER_NAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME_PRODUCTION,
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT, 10),
+    username: DB_USER_NAME,
+    password: DB_PASSWORD,
+    database: DB_NAME_PRODUCTION,
+    host: DB_HOST,
+    port: parseInt(DB_PORT, 10),
     dialect: 'postgres',
   },
 };
